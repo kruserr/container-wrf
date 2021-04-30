8d134983dc2c0e88c87de81354bb11c057343e3b
kruserr <46799551+kruserr@users.noreply.github.com>
2021-04-30 18:37:23 +0000

    update scripts to extract into folder

--- get-geog-complete.sh
+++ get-geog-complete.sh
@@ -1,1 +1,1 @@
-curl -SL https://www2.mmm.ucar.edu/wrf/src/wps_files/geog_complete.tar.gz | tar -xzC WPS_GEOG
+curl -SL https://www2.mmm.ucar.edu/wrf/src/wps_files/geog_complete.tar.gz | tar -xz --strip-components 1 -C WPS_GEOG
--- get-tutorial.sh
+++ get-tutorial.sh
@@ -1,4 +1,4 @@
-curl -SL https://www2.mmm.ucar.edu/wrf/src/wps_files/geog_low_res_mandatory.tar.gz | tar -xzC WPS_GEOG
-curl -SL https://www2.mmm.ucar.edu/wrf/TUTORIAL_DATA/colorado_march16.new.tar.gz | tar -xzC wrfinput
-curl -SL http://www2.mmm.ucar.edu/wrf/src/namelists_v4.0.2.tar.gz | tar -xzC wrfinput
+curl -SL https://www2.mmm.ucar.edu/wrf/src/wps_files/geog_low_res_mandatory.tar.gz | tar -xz --strip-components 1 -C WPS_GEOG
+curl -SL https://www2.mmm.ucar.edu/wrf/TUTORIAL_DATA/colorado_march16.new.tar.gz | tar -xz --strip-components 1 -C wrfinput
+curl -SL http://www2.mmm.ucar.edu/wrf/src/namelists_v4.0.2.tar.gz | tar -xz --strip-components 1 -C wrfinput
 curl -SL http://www2.mmm.ucar.edu/wrf/TUTORIAL_DATA/WRF_NCL_scripts.tar.gz | tar -xzC .
